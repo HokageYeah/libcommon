2c49b466994263138b2b0c4cdd0e3f7551cae15b
yuye <18838280615@163.com>
2023-04-12 14:30:37 +0800

    提交公共类型

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 // todo 12
 export interface User {
-    username: string,
-    password: string,
+  username: string;
+  password: string;
+  userid?: number;
+  newPassword?: string;
+  [key :string]: any
 }
